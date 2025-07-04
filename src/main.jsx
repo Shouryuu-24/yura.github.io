bc8a68c7c4aa8d5ca7ee1f7fe54bb54aa18055fd
Shouryuu-24 <mrakhanaufalls02@gmail.com>
2025-07-05 00:35:57 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Navbar from "./component/navbar.jsx";
-import Footer from "./component/footer.jsx";
+import Navbar from "./components/navbar.jsx";
+import Footer from "./components/footer.jsx";
 import "animate.css";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
@@ -11,7 +11,7 @@ import "aos/dist/aos.css"; // You can also use <link> for styles
 AOS.init();
 
 import "remixicon/fonts/remixicon.css";
-import Preloader from "./component/Preloader.jsx";
+import Preloader from "./components/Preloader.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Preloader />
